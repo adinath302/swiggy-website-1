440a5f8fb8fe063507429c4e759a86fa6922fb90
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-11-17 21:50:04 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="p-[15px] shadow-xl">
+      <div className="max-w-[1200px].mx-auto border border-red-500">
+        <div className="w-[100px]">
+          <img src="images/logo.png" className="w-full" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
